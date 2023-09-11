7dce9ab33f82075ba1b3208ffd8df72859e6359e
jooy <jooy@sigrrow.io>
2023-09-11 18:35:32 +0900

    sample commit

---
+++ src/main/java/com/nhnenterprise/sample/HelloController.java
@@ -0,0 +1,13 @@
+package com.nhnenterprise.sample;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String helloController() {
+        return "Hello";
+    }
+}
